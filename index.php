7c1aa61a124880cb572f85ff18564a580b49e770
ninhphamdinh <122888854+ninhphamdinh@users.noreply.github.com>
2023-01-17 14:59:27 +0100

    Calculator

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
 <?php
+$FirstNumber = $_POST['FirstNumber'];
+$SecondNumber = $_POST['SecondNumber'];
+$Result = $_POST['Result'];
 
-echo "Hello World!";
+    function MyCalculator($Number1,$Number2, $Result) 
+// set $Result as parameter
+    {
+        switch($Result)
+        {
+        case "Addition": 
+// here you have to use colons not semi-colons
+            $compute = $Number1 + $Number2; 
+            break;
+        case "Subtraction":
+            $compute = $Number1 - $Number2; 
+            break;
+        case "Multiplication":
+            $compute = $Number1 * $Number2; 
+            break;
+        case "Division":
+            $compute = $Number1 / $Number2; 
+            break;
+        }
+        return $compute; // returning variable
+    }
+echo "$Result <br /> <br /> 1st Number: $FirstNumber <br /> 2nd Number: $SecondNumber <br /><br />";
+echo "Answer is:" .MyCalculator($FirstNumber,$SecondNumber, $Result);
+ // you need to pass $Result as argument of that function
+?>
